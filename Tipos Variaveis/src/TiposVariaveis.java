e443a1be7c01069a6699598f3b84f824dc7d0762
Bunga António <98521809+bungaantonio@users.noreply.github.com>
2024-08-10 16:19:19 +0100

    commit: Tipos e Variáveis aplicados

--- Tipos Variaveis/src/TiposVariaveis.java
+++ Tipos Variaveis/src/TiposVariaveis.java
@@ -1,5 +1,14 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String titulo = "DIO - BOOTCAMP";
+        int anoCurso = 2024;
+        short numeroCurso = 1;
+        double custoCurso = 0;
+
+        System.out.println(titulo);
+        System.out.println(anoCurso);
+        System.out.println(numeroCurso);
+        System.out.println(custoCurso);
+
     }
 }
